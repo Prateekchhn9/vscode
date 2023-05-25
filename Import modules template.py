c677db16eed42aadfc7824b1af9cd8059a0cb10f
Prateekchhn9 <Prateekchhn9@gmail.com>
2023-05-25 21:56:14 +0200

    this is a new commit for template file to add in git remote repo

---
+++ Import modules template.py
@@ -0,0 +1,30 @@
+# Import necessary modules
+import module1
+import module2
+
+# Define global variables
+
+# Define functions
+
+def main():
+    # Main program logic
+    pass
+
+if __name__ == "__main__":
+    main()
+
+'''
+Let me explain the structure of this template:
+
+Import necessary modules: You can import any required modules at the beginning of your code. Replace module1 and module2 with the actual modules you need.
+
+Define global variables: If you have any global variables that will be used across multiple functions, you can define them here.
+
+Define functions: Define any functions you'll be using in your program. You can define them before the main() function or inside it, depending on your requirements.
+
+Main function: The main() function is where you write the main logic of your program. Replace pass with the actual code you want to execute.
+
+Execute the main function: The if __name__ == "__main__": block ensures that the main() function is executed only when the script is run directly and not when it is imported as a module.
+
+You can customize this template by adding more functions, global variables, or additional module imports based on your specific program requirements.
+'''
