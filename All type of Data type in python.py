8e07796890bc37ca5f0b7b35bef15c43691059e2
Prateekchhn9 <Prateekchhn9@gmail.com>
2023-05-25 21:34:48 +0200

    commiting two files to get VScode repo

---
+++ All type of Data type in python.py
@@ -0,0 +1,41 @@
+#below are all type of Data type in python
+
+#1-Numeric type
+age=25
+pi=3.14
+a=10
+
+#2-Bolean type
+is_living=True
+is_living=False
+
+#3-text type
+name="Prateek Chauhan"
+message="hello,world"
+
+#4-Sequence type
+#list
+Player_name=["Prateek","Abhinav","Prashanthi","Ravi","rahul"]
+
+#Tuple
+coordinates=(10,20)
+
+#Range
+whole_no=range(0, 10)
+print(whole_no)
+
+#Mapping Type
+person={"name":"Prateek","Age":30,"Qualification":"MBA","CGPA":10}
+print(person)
+
+#Set type
+number_set={1,2,5,3,6,8,30,19}
+print(number_set)
+ 
+#Frozen set
+immutable_set = frozenset({1, 2, 3})
+print(immutable_set)
+
+#Null Set
+no_value=None
+print(no_value)
---
+++ Using Input and data sets.py
@@ -0,0 +1,6 @@
+name=input("Please type your name :")
+age=int(input("please type your age :"))
+print("your name is",name,"and your age is" ,age)
+
+MBASubjects= ["Stategic management","ME",'IM','PM','CF']
+print('Here are your MBA Subjects :' , MBASubjects)
